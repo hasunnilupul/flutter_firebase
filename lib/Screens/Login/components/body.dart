7767e8977da6b20e3df7ef5b9dabca8670f4cc9c
Hasun Nilupul <hasunnilupul16@gmail.com>
2021-08-22 09:01:26 +0530

    Add SingleChildScrollView to Login Page

--- lib/Screens/Login/components/body.dart
+++ lib/Screens/Login/components/body.dart
@@ -15,36 +15,38 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'LOGIN',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: size.height * 0.03,
-          ),
-          SvgPicture.asset(
-            'assets/icons/login.svg',
-            height: size.height * 0.4,
-          ),
-          RoundedInputField(
-            hintText: 'Your email',
-            icon: Icons.person,
-            onChanged: (text) {},
-          ),
-          RoundedPasswordField(
-            onChanged: (text) {},
-          ),
-          RoundedButton(text: 'LOGIN', press: () {}),
-          SizedBox(
-            height: size.height * 0.03,
-          ),
-          AlreadyHaveAnAccountCheck(
-            press: () {},
-          )
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'LOGIN',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: size.height * 0.03,
+            ),
+            SvgPicture.asset(
+              'assets/icons/login.svg',
+              height: size.height * 0.4,
+            ),
+            RoundedInputField(
+              hintText: 'Your email',
+              icon: Icons.person,
+              onChanged: (text) {},
+            ),
+            RoundedPasswordField(
+              onChanged: (text) {},
+            ),
+            RoundedButton(text: 'LOGIN', press: () {}),
+            SizedBox(
+              height: size.height * 0.03,
+            ),
+            AlreadyHaveAnAccountCheck(
+              press: () {},
+            )
+          ],
+        ),
       ),
     );
   }
